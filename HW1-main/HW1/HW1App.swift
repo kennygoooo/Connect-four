d0686bd649cf7351c998a3ec92c55b979a7db343
Kenny <91319666+kennygoooo@users.noreply.github.com>
2022-06-30 00:30:08 +0800

    Add files via upload

---
+++ HW1-main/HW1/HW1App.swift
@@ -0,0 +1,17 @@
+//
+//  HW1App.swift
+//  HW1
+//
+//  Created by Kenny on 2022/5/15.
+//
+
+import SwiftUI
+
+@main
+struct HW1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
